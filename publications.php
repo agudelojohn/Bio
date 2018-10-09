a3b20c759b0bc90f601efdbfb486ea27d3acdb65
agudelojohn <jhbr08@hotmail.com>
2018-10-09 18:45:34 -0500

    Carousel de imagenes en Publications

---
+++ publications.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio de la app</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="navBar/navBar.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+ 
+</head>
+<body>
+<?php
+include("navBar/navbar.html");
+?>    
+
+<style>
+    .carousel-item{
+        max-height:800px;
+        min-height:700px;
+        overflow:hidden;
+    }
+    .carousel-item img{
+        width:100% !important;
+    }
+
+</style>
+
+<!-- carousel -->
+<div class="row justify-content-md-center">
+    <div class="col col-lg-auto" id=contenedorSlide>
+      <!-- corusel -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="imgs/Internas/grupo1.png" alt="Imagen del grupo 1">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="imgs/Internas/grupo2.png" alt="Imagen del grupo 2">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+
+<!-- until here -->
+
+
+
+<?php
+include("navBar/footer.html");
+?>    
+</div>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
