7b20ed5d751affb1b797de9fe0c7bf811197b1d0
agudelojohn <jhbr08@hotmail.com>
2018-10-26 10:22:40 -0500

    Actualicé los registros de Teaching

---
+++ teaching.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio de la app</title>
+
+    <script src="Bjs/jquery.js"></script>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="Bcss/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="navBar/navBar.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+ 
+</head>
+<body>
+<?php
+include("navBar/navbar.html");
+?> 
+   
+<div class="row justify-content-md-center" style="margin-top:20px;"> <!-- row to keep know where are you -->
+    <div class="col col-lg-11" > 
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page"><a href="ryt.php">Teaching</a></li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<?PHP
+
+// desde aquí la conexión a la BD
+require("php/conexion.php");
+// sentencia
+$sql="select * from people_profiles join clases on clases.profesor=people_profiles.id_profile;";
+// query para mandar la sentencia
+$resultado = mysqli_query($conexion,$sql);
+// toma los datos en un array
+while($array=mysqli_fetch_array($resultado)){
+    echo '
+    <div class="container">
+        <div class="row ">
+            <div class="col col-lg-4" > 
+                <img src="'.$array['img_profile'].'" style="width:100%;">
+            </div>
+            <div class="col col-lg-auto" > 
+                <h1><strong>'.$array['name'].'</strong></h1><br>
+                <p>
+                    <strong>'.$array['nombre_clase'].'</strong><br>
+                    '.$array['descripcion'].'<br>'.$array['aula'].'<br>
+                    '.$array['horario'].'
+                </p>
+            </div>
+        </div>
+    </div>';
+}
+?>
+
+
+<?php
+include("navBar/footer.html");
+?>    
+</div>
+
+<script src="Bjs/bootstrap.min.js"></script>
+</body>
+</html>
