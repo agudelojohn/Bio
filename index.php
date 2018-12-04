e04724c0e3688f03f1e97ace506a0eb4c31f4d02
agudelojohn <jhbr08@hotmail.com>
2018-12-03 22:53:10 -0500

    update in news

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include("navBar/navbar.html");
             <div class="card" style="width:100%; height:100%;">
                 <div class="card-body">
                     <h5 class="card-title">Biomedical Imaging, Vision and Learning Laboratory</h5>
-                    <p class="card-text">BivL2ab is mainly dedicated to solve problems related with visual information, covering from the acquisition and analysis of medical images to the understanding of complex spatio-temporal patterns in the general context of computer vision.  Such problems are tipically highly variant and challenging, being hence, the proposal of learning strategies fundamental to stand out significant behaviors from observed data, and to model prior expert knowledge in robust representations.</p>
+                    <p class="card-text">BivLab is mainly dedicated to solve problems related with visual information, covering from the acquisition and analysis of medical images to the understanding of complex spatio-temporal patterns in the general context of computer vision.  Such problems are tipically highly variant and challenging, being hence, the proposal of learning strategies fundamental to stand out significant behaviors from observed data, and to model prior expert knowledge in robust representations.</p>
                 </div>
             </div>
         </div>
@@ -58,13 +58,38 @@ $sql="Select*from news order by counter;";
 $resultado = mysqli_query($conexion,$sql);
 // toma los datos en un array
 $contador=1;
-while($array=mysqli_fetch_array($resultado)){
-    if($array['counter']!=0)
-    {                
-        if($contador==1){
-            echo'
-            <div class="row justify-content-md-center" style="margin-top:20px;">
-                <div class="col col-lg-5" >
+
+$sql2="Select count(*) from news order by counter;";
+$resultado2 = mysqli_query($conexion,$sql2);
+$cantidad= mysqli_fetch_assoc($resultado2); 
+
+if($cantidad['count(*)'] != 0){
+    while($array=mysqli_fetch_array($resultado)){
+        if($array['counter']!=0)
+        {                
+            if($contador==1){
+                echo'
+                <div class="row justify-content-md-center" style="margin-top:20px;">
+                    <div class="col col-lg-5" >
+                        <div class="jumbotron jumbotron-fluid" id=jum-1>
+                            <div class="row justify-content-md-center">
+                                <div class="col col-lg-10" >
+                                    <h1 class="display-4 display-news">'.$array['title'].'</h1>
+                                    <p class="lead">'.$array['date_new'].'</p>
+                                </div>
+                                <div class="col col-lg-1 col-btn" >
+                                    <a href="'.$array['url'].'" class="btn btn-primary btn-lg active btn-news" role="button" aria-pressed="true" style="padding-top:40px;">></a>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                
+                ';
+                $contador=0;
+            }else
+                {
+                echo'
+                    <div class="col col-lg-5" >
                     <div class="jumbotron jumbotron-fluid" id=jum-1>
                         <div class="row justify-content-md-center">
                             <div class="col col-lg-10" >
@@ -76,35 +101,28 @@ while($array=mysqli_fetch_array($resultado)){
                             </div> 
                         </div>
                     </div>
-                </div>
-            
-            ';
-            $contador=0;
-        }else
-            {
-            echo'
-                <div class="col col-lg-5" >
-                <div class="jumbotron jumbotron-fluid" id=jum-1>
-                    <div class="row justify-content-md-center">
-                        <div class="col col-lg-10" >
-                            <h1 class="display-4 display-news">'.$array['title'].'</h1>
-                            <p class="lead">'.$array['date_new'].'</p>
-                        </div>
-                        <div class="col col-lg-1 col-btn" >
-                            <a href="'.$array['url'].'" class="btn btn-primary btn-lg active btn-news" role="button" aria-pressed="true" style="padding-top:40px;">></a>
-                        </div> 
                     </div>
-                </div>
-                </div>
-            </div>';
-            $contador=1;
-            }
+                </div>';
+                $contador=1;
+                }
+        }
+    }
+
+    if($contador==0)
+    {
+        echo'</div>';
     }
 }
-
-if($contador==0)
-{
-    echo'</div>';
+else{
+    echo '<div class="row justify-content-md-center" style="margin-top:20px;"> <!-- row to keep know where are you -->
+    <div class="col col-lg-10" > 
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page"><a href="manage.php">News are empty, please include one. Click here.</a></li>
+            </ol>
+        </nav>
+    </div>
+</div>';
 }
 ?>
 
