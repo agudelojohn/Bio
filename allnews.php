46c5a3b863aaaa17adf4a75aca2c9aac003d39e6
agudelojohn <jhbr08@hotmail.com>
2019-01-25 08:56:17 -0500

    news working

--- allnews.php
+++ allnews.php
@@ -35,7 +35,97 @@ if(isset($_SESSION['acceso']))
 } 
 ?>    
 
+<div class="row justify-content-md-center" style="margin-top:20px;"> <!-- row to keep know where are you -->
+            <div class="col col-lg-11" > 
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page"><a href="ryt.php">News</a></li>
+                    </ol>
+                </nav>
+            </div>
+</div>
+<?php
+// desde aquí la conexión a la BD
+require("php/conexion.php");
+// sentencia
+$sql="Select*from news order by date_new;";
+// query para mandar la sentencia
+$resultado = mysqli_query($conexion,$sql);
+// toma los datos en un array
+while($array=mysqli_fetch_array($resultado)){
+    // antes del echo dividiré los arrays en sus respectivos strings
+    $title=$array['title'];
+    $date=$array['date_new'];
+    $content=$array['content'];
+    $img=$array['img_news'];
 
+echo '
+        <!-- Previo a las noticias -->
+        <div class=container>
+            <div class="row justify-content-md-center ">
+                <div class="col col-lg-12 " align=center>
+                <!-- aqui va la imagen -->
+                    <img class="img_new" src="'.$img.'" alt="" style="height:100%;">
+                </div>
+            </div>
+            <div class="row justify-content-md-center ">
+            <div class="col col-lg-1" >
+            </div>
+                <div class="col col-lg-9" >
+                    <h5>'.$date.'</h5><h1>'.$title.'</h1>
+                </div>
+                <div class="col col-lg-2" style="margin:auto;" align=center>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-'.$date.'">
+                    Complete data
+                    </button>
+                </div>
+            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-info" style="margin-bottom:20px;"></div>
+        <!-- // ------------------------------------>
+        <!-- MODALS -->
+        <!-- ------------------------------------ -->
+        
+        
+        <!-- Modal -->
+        <div class="modal fade" id="modal-'.$date.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h3 class="modal-title" id="exampleModalLongTitle">'.$title.'</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="row justify-content-center">
+                    <div class="col col-lg-12">
+                        <img class="img-profile " src="'.$img.'" alt="">
+                    </div>
+                    <div class="col col-lg-12">
+                        <h5>'.$date.'</h5>
+                        <p>'.$content.'</p>            
+                    </div>
+                </div>
+                
+                </div>
+            </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+';
+}
+// echo '<br><br>';
+?>
 
 
 
